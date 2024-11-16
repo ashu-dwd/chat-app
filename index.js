2a4cbf65be10f08dce2fb8c4a6761a451e9b9c60
raghav-dwd <dwivediji425@gmail.com>
2024-11-16 18:01:34 +0530

    improved

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const { Server } = require("socket.io");
 
 const app = express();
 const server = http.createServer(app);
-const port = 3000;
+const port = process.env.PORT || 3000;
 const io = new Server(server);
 
 // socket.io
